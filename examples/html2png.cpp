8903768496078e83a51180e7f734ef3038571289
Samuel Ugochukwu <sammycageagle@gmail.com>
2025-05-16 11:48:43 +0100

    Remove `html2png` example

--- examples/html2png.cpp
+++
@@ -1,9 +0,0 @@
-#include "plutobook.hpp"
-
-int main()
-{
-    plutobook::Book book(plutobook::PageSize::A4);
-    book.loadUrl("/home/sammycage/Projects/hello.html");
-    book.writeToPng("hello.png");
-    return 0;
-}
