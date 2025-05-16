3d1fff3e5f3baaece38fec3d7e9cb8ad95352225
Samuel Ugochukwu <sammycageagle@gmail.com>
2025-05-16 13:03:53 +0100

    Update `html2pdf.cpp`

--- examples/html2pdf.cpp
+++ examples/html2pdf.cpp
@@ -1,9 +1,37 @@
 #include "plutobook.hpp"
 
-int main()
+#include <iostream>
+
+int main(int argc, char* argv[])
 {
+    if (argc != 3 && argc != 4) {
+        std::cerr << "Usage:\n"
+                  << "  html2pdf <input> <output> [<style>]\n\n"
+                  << "Arguments:\n"
+                  << "  input     URL or filename of the HTML input\n"
+                  << "  output    Path to the output PDF file\n"
+                  << "  style     Optional CSS style string to apply (e.g., \"@page { size: A4; }\")\n";
+        return 1;
+    }
+
+    const char* input = argv[1];
+    const char* output = argv[2];
+    const char* style = "";
+    if(argc == 4) {
+        style = argv[3];
+    }
+
     plutobook::Book book(plutobook::PageSize::A4);
-    book.loadUrl("/home/sammycage/Projects/hello.html");
-    book.writeToPdf("hello.pdf");
+    if(!book.loadUrl(input, style)) {
+        std::cerr << "Error: Failed to load input HTML: \"" << input << "\"" << std::endl;
+        return 2;
+    }
+
+    if(!book.writeToPdf(output)) {
+        std::cerr << "Error: Failed to write PDF to output file: \"" << output << "\"" << std::endl;
+        return 3;
+    }
+
+    std::cout << "Generated PDF file: " << output << std::endl;
     return 0;
 }
