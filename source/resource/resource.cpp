afae944bb3ea73259a1b90b312c1eb05a3fb1b26
Samuel Ugochukwu <sammycageagle@gmail.com>
2025-08-08 18:22:07 +0100

    Add conditional compilation for `CURLSSLOPT_NATIVE_CA` to support older versions

--- source/resource/resource.cpp
+++ source/resource/resource.cpp
@@ -258,7 +258,9 @@ ResourceData DefaultResourceFetcher::fetchUrl(const std::string& url)
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, content);
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "PlutoBook/" PLUTOBOOK_VERSION_STRING);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+#ifdef CURLSSLOPT_NATIVE_CA
     curl_easy_setopt(curl, CURLOPT_SSL_OPTIONS, CURLSSLOPT_NATIVE_CA);
+#endif
     auto response = curl_easy_perform(curl);
     if(response == CURLE_OK) {
         const char* contentType = nullptr;
