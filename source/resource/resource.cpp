fd217500272859d8230a4d5b3aefa2d39506029f
Samuel Ugochukwu <sammycageagle@gmail.com>
2025-12-09 08:56:10 +0100

    Refactor SSL verification settings in curl

--- source/resource/resource.cpp
+++ source/resource/resource.cpp
@@ -285,8 +285,8 @@ ResourceData DefaultResourceFetcher::fetchUrl(const std::string& url)
         curl_easy_setopt(curl, CURLOPT_CAPATH, m_caPath.data());
     }
 
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, m_verifyPeer);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, m_verifyHost);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, m_verifyPeer ? 2L : 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, m_verifyHost ? 1L : 0L);
 #ifdef CURLSSLOPT_NATIVE_CA
     curl_easy_setopt(curl, CURLOPT_SSL_OPTIONS, CURLSSLOPT_NATIVE_CA);
 #endif
