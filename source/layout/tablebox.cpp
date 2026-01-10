936b128eb5ad9ba5cf6661578ef4c1ceba8a4006
Samuel Ugochukwu <sammycageagle@gmail.com>
2026-01-10 09:58:03 +0100

    Refactor `TableSectionBox::paintCollapsedBorders`

--- source/layout/tablebox.cpp
+++ source/layout/tablebox.cpp
@@ -1433,15 +1433,11 @@ void TableSectionBox::build()
 
 void TableSectionBox::paintCollapsedBorders(const PaintInfo& info, const Point& offset, const TableCollapsedBorderEdge& currentEdge) const
 {
-    const auto printing = view()->currentPage();
     for(auto row : m_rows | std::views::reverse) {
         Point adjustedOffset(offset + location() + row->location());
         for(const auto& [col, cell] : row->cells()) {
             auto cellBox = cell.box();
             if(!cell.inColOrRowSpan()) {
-                const auto& rect = info.rect();
-                if(printing && rect.y > adjustedOffset.y + cellBox->y())
-                    continue;
                 cellBox->paintCollapsedBorders(info, adjustedOffset, currentEdge);
             }
         }
